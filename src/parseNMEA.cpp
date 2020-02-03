7d144a439b446c3bcaa9e033007e063af9b3cc83
amich1 <49416568+amich1@users.noreply.github.com>
2020-02-03 13:08:43 +0000

    Changed indentaion from tabs to spaces

--- src/parseNMEA.cpp
+++ src/parseNMEA.cpp
@@ -6,34 +6,34 @@ namespace NMEA
 
   bool isWellFormedSentence(std::string)
   {
-      // Stub definition, needs implementing#
-      // Jamie Will Do this
-      return false;
+    // Stub definition, needs implementing#
+    // Jamie Will Do this
+    return false;
   }
 
   bool hasValidChecksum(std::string)
   {
-      // Stub definition, needs implementing
-      // Jamie Will Do this
-      return false;
+    // Stub definition, needs implementing
+    // Jamie Will Do this
+    return false;
   }
 
   SentenceData extractSentenceData(std::string)
   {
-      // Stub definition, needs implementing
-      return {"",{}};
+    // Stub definition, needs implementing
+    return {"",{}};
   }
 
   GPS::Position positionFromSentenceData(SentenceData)
   {
-      // Stub definition, needs implementing
-      return GPS::Earth::NorthPole;
+    // Stub definition, needs implementing
+    return GPS::Earth::NorthPole;
   }
 
   Route routeFromLog(std::istream &)
   {
-      // Stub definition, needs implementing
-      return {};
+    // Stub definition, needs implementing
+    return {};
   }
 
 }
