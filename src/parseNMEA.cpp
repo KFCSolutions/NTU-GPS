01f9430a5764844177995929cf62e2a7616a4427
amich1 <49416568+amich1@users.noreply.github.com>
2020-02-03 16:57:49 +0000

    first try at getting the extractor to work

--- src/parseNMEA.cpp
+++ src/parseNMEA.cpp
@@ -18,10 +18,45 @@ namespace NMEA
     return false;
   }
 
-  SentenceData extractSentenceData(std::string)
+  SentenceData extractSentenceData(std::string sen)
   {
-    // Stub definition, needs implementing
-    return {"",{}};
+    // prefixLen = length of NMEA prefix ($GP)
+    // suffixLen = length of checksum suffix (*FF)
+    // formatLen = length of sentance format string (GSV)
+    const int prefixLen = 3;
+    const int suffixLen = 3;
+    const int formatLen = 3;
+
+    // variables used for operations 
+    string formatRet, fieldsStr;
+    formatRet = nmea;
+
+    // remove prefix from formatRet & assign it to fieldsStr
+    formatRet.erase(0, prefixLen); 
+    fieldsStr = formatRet;
+
+    // remove everything that is not the format string from formatRet
+    formatRet.erase(formatLen, string::npos); 
+
+    //remove the format string and the suffix from fieldsStr
+    fieldsStr.erase(0, formatLen + 1);
+    fieldsStr.erase(fieldsStr.length() - suffixLen);
+
+    vector<string> fieldsRet;
+
+    // iterate fieldsStr to populate fieldsRet
+    stringstream s_stream(fieldsStr);
+    while(s_stream.good()) {
+      string substr;
+      getline(s_stream, substr, ',');
+      fieldsRet.push_back(substr);
+    }
+
+    // use formatRet and fieldsRet to init the return value
+    SentanceData ret;
+    ret.first = formatRet;
+    ret.second = fieldsRet;
+    return ret;
   }
 
   GPS::Position positionFromSentenceData(SentenceData)
